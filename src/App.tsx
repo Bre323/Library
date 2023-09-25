5898fc52a416857e0a05d7fff86e2b4abdcb66e5
Breno Rocha <brenopenarocha2@gmail.com>
2023-09-25 20:16:12 -0300

    Adding handlechange

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,35 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const myLibrary = [];
-  const [name, setName] = useState('');
-  const [author, setAuthor] = useState('');
-  const [pages, setPages] = useState('');
-  const [status, setStatus] = useState(false);
+
+  let myLibrary: Array<object> = [];  
+  let mainContainer = document.getElementById('main-container');
+  const [bookInfo, setBookInfo] = useState({
+    name: '',
+    author: '',
+    pages: 0,
+    status: 'not-read'
+  })
 
 
-  const addBook = () => {}
-  const removeBook = () => {}
+  const handleChange = (event: any) => {
+    const updatedBookInfo = {
+      ...bookInfo,
+      [event.target.name]: event.target.value
+    }
+
+    setBookInfo(updatedBookInfo);
+    console.log(updatedBookInfo);
+  }
+
+
+  const addBook = () => {
+
+  }
+
+  const removeBook = () => {
+
+  }
 
 
   return (
@@ -18,35 +38,35 @@ function App() {
       <header>
         <h1>Book Library</h1>
 
-        <form action="post" className='flex'>
+        <div className='header-container flex'>
           <div className='bookname flex'>
             <label htmlFor="bookname-input">Book</label>
-            <input type="text" id='bookname-input' />
+            <input type="text" id='bookname-input' name='name' value={bookInfo.name} onChange={handleChange} required />
           </div>
 
           <div className='author flex'>
             <label htmlFor="author-input">Author</label>
-            <input type="text" id='author-input' />
+            <input type="text" id='author-input' name='author' value={bookInfo.author} onChange={handleChange} required />
           </div>
 
           <div className='pages flex'>
             <label htmlFor="pages-input">Pages</label>
-            <input type="number" id='pages-input' />
+            <input type="number" id='pages-input' name='pages' value={bookInfo.pages} onChange={handleChange} required />
           </div>
 
           <div className='status flex'>
             <label htmlFor="status-input">Status</label>
-            <select name="status" id="status-input">
-              <option value="read">Read</option>
+            <select id="status-input" name="status" value={bookInfo.status} onChange={handleChange} required >
+              <option value="read" selected>Read</option>
               <option value="not-read">Not Read</option>
             </select>
           </div>
 
           <input type="submit" className='add-book' onClick={addBook} />
-        </form>
+        </div>
       </header>
 
-      <main></main>
+      <main id='main-container'></main>
     </>
   )
 }
